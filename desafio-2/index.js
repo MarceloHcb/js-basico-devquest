145cd96d0f6fcdd793662b33d480c236d268152b
MarceloHcb <marcelohcb@msn.com>
2021-12-16 13:37:00 -0300

    desafio basico js

---
+++ desafio-2/index.js
@@ -0,0 +1,8 @@
+var boletoPago = false
+
+if(boletoPago===true){
+    alert("O boleto está pago")
+}else{
+    alert("O boleto não está pago")
+}
+
---
+++ desafio-5/index.js
@@ -0,0 +1,7 @@
+for (let i=0; i<=10; i++){
+    console.log(" O dobro de " + i + " é "+ (2*i) )
+    let multPeloDobro=i *(i*2)
+    console.log(multPeloDobro)
+
+}
+   
