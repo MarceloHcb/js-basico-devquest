63d01b0836eb8e2dba50f91718fd01fa23c12606
MarceloHcb <marcelohcb@msn.com>
2021-12-20 10:16:51 -0300

    upgrade

--- desafio-5/index.js
+++ desafio-5/index.js
@@ -1,7 +1,5 @@
 for (let i=0; i<=10; i++){
-    console.log(" O dobro de " + i + " é "+ (2*i) )
-    let multPeloDobro=i *(i*2)
-    console.log(multPeloDobro)
 
+    console.log(" O dobro de " + i + " é "+ (2*i) )
 }
    
