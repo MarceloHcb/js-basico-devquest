832984e8b41777374fa296e6c87d94ec367fa57c
MarceloHcb <marcelohcb@msn.com>
2021-12-20 10:22:06 -0300

    upgrade

--- desafio-6/index.js
+++ desafio-6/index.js
@@ -1,33 +1,33 @@
-function somar(numero1,numero2){
-    let soma=numero1 + numero2 ;
-    return("A soma dos números é: "+soma)
+function somar(numero1, numero2) {
+    let soma = numero1 + numero2;
+    return ("A soma dos números é: " + soma)
 }
 
-function subtracao(numero1,numero2){
-    let subtrair=numero1 - numero2 ;
-    return("A subtração dos números é: " + subtrair)
+function subtracao(numero1, numero2) {
+    let subtrair = numero1 - numero2;
+    return ("A subtração dos números é: " + subtrair)
 }
 
-function multiplicacao(numero1,numero2){
-    let multiplicar=numero1 * numero2 ;
-    return ("A multiplicação dos números é: "+ multiplicar)
+function multiplicacao(numero1, numero2) {
+    let multiplicar = numero1 * numero2;
+    return ("A multiplicação dos números é: " + multiplicar)
 
 }
 
-function divisao(numero1,numero2){
-    let dividir=numero1 / numero2 ;
+function divisao(numero1, numero2) {
+    let dividir = numero1 / numero2;
     return ("A divisão dos números é: " + dividir)
 }
 
-var somando =  somar(3,2) ;
+const somando = somar(3, 2);
 console.log(somando)
 
-var subtraindo = subtracao(3,2)
+const subtraindo = subtracao(3, 2);
 console.log(subtraindo)
 
-var multiplicando = multiplicacao(3,2)
+const multiplicando = multiplicacao(3, 2);
 console.log(multiplicando)
 
-var dividindo = divisao(3,2)
+const dividindo = divisao(3, 2);
 console.log(dividindo)
 
